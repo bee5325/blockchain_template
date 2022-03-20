fe1ed6e9e66fcb1e9eb608bfe25d50c5a7a599af
bee5325 <tanyubin1993@hotmail.com>
2022-03-20 23:08:40 +0800

    Updated test file

---
+++ contract/test/sample.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("sample", () => {
+  describe("deployment", () => {
+    it("should deploy", async () => {});
+  });
+});
